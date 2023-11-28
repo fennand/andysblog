91fd1c1f0f4282f3df16ca98e10c4f0f8ce93d70
Andrew Fennell <afennell@hotmail.co.uk>
2023-11-28 17:06:39 +0000

    fix spelling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Andy's Blog Page! The bloggiest on the internet!</h1>
+      <h1>Andy&apos;s Blog Page! The bloggiest on the internet!</h1>
     </main>
   );
 }
